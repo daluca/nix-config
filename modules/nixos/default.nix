87c2af15dee8754e729e1c0f8f12f9c58732de0a
Lucas Slebos <lucas@slebos.nz>
2024-06-01 12:48:59 +1200

    add automated nix garbage collection

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -8,6 +8,12 @@
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 30d";
+  };
+
   time.timeZone = "Pacific/Auckland";
 
   users.users.daluca = {
