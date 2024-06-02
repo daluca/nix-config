a08338de116d7907defab58462b0c8a41cd37e77
Lucas Slebos <lucas@slebos.nz>
2024-06-02 14:42:19 +1200

    add nixos-unstable channel and overlay

--- flake.nix
+++ flake.nix
@@ -4,6 +4,8 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
 
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+
     home-manager = {
       url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -14,6 +16,7 @@
     inputs@{
       self,
       nixpkgs,
+      nixpkgs-unstable,
       home-manager,
       ...
     }:
@@ -26,6 +29,13 @@
       nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
         inherit system;
         modules = [
+          (
+            { config, pkgs, ... }:
+            {
+              nixpkgs.overlays = [ (self: super: { unstable = import nixpkgs-unstable { inherit system; }; }) ];
+            }
+          )
+
           ./hosts/thinkpad
 
           home-manager.nixosModules.home-manager
