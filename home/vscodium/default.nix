697246cceba801c125f4087dc397bb5707ce9c49
Lucas Slebos <lucas@slebos.nz>
2024-06-04 16:08:51 +1200

    add vscode extensions

--- home/vscodium/default.nix
+++ home/vscodium/default.nix
@@ -4,5 +4,20 @@
   programs.vscode = {
     enable = true;
     package = pkgs.unstable.vscodium;
+    enableUpdateCheck = false;
+    enableExtensionUpdateCheck = false;
+    mutableExtensionsDir = false;
+    extensions = with pkgs.unstable.vscode-extensions; [
+      vscodevim.vim
+      jnoortheen.nix-ide
+      github.github-vscode-theme
+    ];
+    userSettings = {
+      workbench.colorTheme = "GitHub Dark";
+    };
+  };
+
+  programs.zsh.shellAliases = {
+    code = "codium";
   };
 }
