d7cfc8fb53a46683fed93e13c059f55ab891c0b3
Lucas Slebos <lucas@slebos.nz>
2024-06-06 12:44:48 +1200

    add catppuccin-mocha skin to k9s

--- home/k9s/default.nix
+++ home/k9s/default.nix
@@ -2,7 +2,24 @@
 
 {
   programs.k9s = {
-    enable = false;
+    enable = true;
     package = pkgs.unstable.k9s;
+    settings.k9s = {
+      ui = {
+        enableMouse = false;
+        logoless = true;
+        skin = "catppuccin-mocha";
+      };
+      skipLatestRevCheck = true;
+    };
   };
+
+  xdg.configFile."k9s/skins".source =
+    pkgs.fetchFromGitHub {
+      owner = "catppuccin";
+      repo = "k9s";
+      rev = "e08b9302aee2b7193d6fcc3a9ffd2f24d2957dbf";
+      hash = "sha256-tg+xFNj9wkWFcD9bXDor/VCZVosoAH+8grz5FY5QgnM=";
+    }
+    + "/dist/";
 }
