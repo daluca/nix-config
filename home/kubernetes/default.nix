fa4a574d27e65d94019c3cbea92a000396f7d8f8
Lucas Slebos <lucas@slebos.nz>
2024-09-08 12:58:07 +1200

    add headscale alias

--- home/kubernetes/default.nix
+++ home/kubernetes/default.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ config, lib, ... }:
 
 {
   imports = [
@@ -6,4 +6,8 @@
     ./helm
     ./k9s
   ];
+
+  programs.zsh.shellAliases = lib.mkIf config.programs.zsh.enable {
+    headscale = "kubectl --context do-syd1-production-cluster --namespace vpn exec -it deployments/headscale -c headscale -- headscale --config=/headscale/config/config.yaml";
+  };
 }
