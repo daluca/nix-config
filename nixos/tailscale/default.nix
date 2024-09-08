430ed3f7f2f9a1e43994123fca2c2fe1dcf59985
Lucas Slebos <lucas@slebos.nz>
2024-09-08 12:57:33 +1200

    add tailscale persistence and set flags

--- nixos/tailscale/default.nix
+++ nixos/tailscale/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ config, lib, pkgs, ... }:
 
 {
   services.tailscale = {
@@ -6,5 +6,14 @@
     package = pkgs.unstable.tailscale;
     openFirewall = true;
     useRoutingFeatures = "both";
+    extraSetFlags = [
+      "--accept-routes"
+      "--advertise-exit-node"
+      "--advertise-routes=10.0.0.0/14"
+    ];
   };
+
+  environment.persistence.system.directories = lib.mkIf config.environment.persistence.system.enable [
+    { directory = "/var/lib/tailscale"; mode = "0700"; }
+  ];
 }
