4bde1ebb39b802e58b69d9859d1a126c1dd242cc
Lucas Slebos <lucas@slebos.nz>
2024-09-10 11:23:15 +1200

    update tailscale flags to be for a client

--- nixos/tailscale/default.nix
+++ nixos/tailscale/default.nix
@@ -5,14 +5,14 @@
     enable = true;
     package = pkgs.unstable.tailscale;
     openFirewall = true;
-    useRoutingFeatures = "both";
+    useRoutingFeatures = "client";
     authKeyFile = config.sops.secrets.tailscale-preauthkey.path;
     extraUpFlags = [
       "--login-server=http://headscale.${secrets.cloud.domain}"
       "--operator=${config.home-manager.users.daluca.home.username}"
       "--accept-routes"
-      "--advertise-exit-node"
-      "--advertise-routes=10.0.0.0/14"
+      "--accept-dns"
+      "--shields-up"
       "--hostname=${config.networking.hostName}"
       "--reset"
     ];
