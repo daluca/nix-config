42a9cd89b50bfcd05c790ffd7da0b37b4f6b36bc
Lucas Slebos <lucas@slebos.nz>
2024-09-09 14:21:00 +1200

    add tailscale preauthkey

--- nixos/tailscale/default.nix
+++ nixos/tailscale/default.nix
@@ -1,4 +1,4 @@
-{ config, lib, pkgs, ... }:
+{ config, lib, pkgs, secrets, ... }:
 
 {
   services.tailscale = {
@@ -6,12 +6,19 @@
     package = pkgs.unstable.tailscale;
     openFirewall = true;
     useRoutingFeatures = "both";
-    extraSetFlags = [
+    authKeyFile = config.sops.secrets.tailscale-preauthkey.path;
+    extraUpFlags = [
+      "--login-server=http://headscale.${secrets.cloud.domain}"
+      "--operator=${config.home-manager.users.daluca.home.username}"
       "--accept-routes"
       "--advertise-exit-node"
       "--advertise-routes=10.0.0.0/14"
+      "--hostname=${config.networking.hostName}"
+      "--reset"
     ];
   };
+
+  sops.secrets.tailscale-preauthkey = { };
 
   environment.persistence.system.directories = lib.mkIf config.environment.persistence.system.enable [
     { directory = "/var/lib/tailscale"; mode = "0700"; }
