640effaf45a6847f6caae157be0e1f0887af6ed0
Lucas Slebos <lucas@slebos.nz>
2024-09-30 12:50:13 +1300

    add zsh-nix-shell plugin

--- home/zsh/default.nix
+++ home/zsh/default.nix
@@ -1,4 +1,4 @@
-{ config, osConfig, ... }:
+{ config, pkgs, osConfig, ... }:
 
 {
   programs.zsh = rec {
@@ -27,6 +27,18 @@
     enableCompletion = true;
     autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
+    plugins = [
+      {
+        name = "zsh-nix-shell";
+        file = "nix-shell.plugin.zsh";
+        src = pkgs.fetchFromGitHub {
+          owner = "chisui";
+          repo = "zsh-nix-shell";
+          rev = "v0.8.0";
+          hash = "sha256-Z6EYQdasvpl1P78poj9efnnLj7QQg13Me8x1Ryyw+dM=";
+        };
+      }
+    ];
     oh-my-zsh = {
       enable = true;
       plugins = [
