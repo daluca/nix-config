954804079d7434143a6bcf3cd85d950585d75fdb
Lucas Slebos <lucas@slebos.nz>
2024-09-30 13:05:58 +1300

    add kubernetes remote command aliases

--- home/kubernetes/default.nix
+++ home/kubernetes/default.nix
@@ -9,5 +9,7 @@
 
   programs.zsh.shellAliases = lib.mkIf config.programs.zsh.enable {
     headscale = "kubectl --context do-syd1-production-cluster --namespace vpn exec -it deployments/headscale -c headscale -- headscale --config=/headscale/config/config.yaml";
+    cscli = "kubectl --context do-syd1-production-cluster --namespace crowdsec exec -it deployments/crowdsec-lapi -- cscli";
+    ntfy = "kubectl --context do-syd1-production-cluster --namespace ntfy exec -it statefulsets/ntfy -- ntfy";
   };
 }
