a8d67298d83a342f064e992c13760ad3130dada6
Lucas Slebos <lucas@slebos.nz>
2024-09-30 13:39:43 +1300

    add custom locales

--- nixos/common/default.nix
+++ nixos/common/default.nix
@@ -26,6 +26,19 @@
 
   time.timeZone = "Pacific/Auckland";
 
+  i18n = {
+    defaultLocale = "en_NZ.UTF-8";
+    supportedLocales = unique (builtins.map (locale: (replaceStrings ["utf8" "utf-8" "UTF8"] ["UTF-8" "UTF-8" "UTF-8"] locale) + "/UTF-8") (
+    [
+      "C.UTF-8"
+      "en_GB.UTF-8"
+      "en_US.UTF-8"
+      "en_AU.UTF-8"
+      config.i18n.defaultLocale
+    ] ++ (attrValues (filterAttrs (n: v: n != "LANGUAGE") config.i18n.extraLocaleSettings))
+    ));
+  };
+
   hardware.pulseaudio.enable = true;
   sound.enable = true;
 }
