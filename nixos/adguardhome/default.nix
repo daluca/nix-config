caa2c152526bf4f0734baca138573795af98c06f
Lucas Slebos <lucas@slebos.nz>
2024-10-06 19:45:56 +1300

    add port and bind host settings

--- nixos/adguardhome/default.nix
+++ nixos/adguardhome/default.nix
@@ -9,12 +9,17 @@
     enable = true;
     mutableSettings = false;
     openFirewall = true;
+    port = 80;
     settings = {
       users = [{
         name = config.home-manager.users.daluca.home.username;
         password = secrets.adguardhome.password;
       }];
       dns = {
+        bind_hosts = [
+          "127.0.0.1"
+          "10.3.167.29"
+        ];
         upstream_dns = [
           "127.0.0.1:5353"
         ];
