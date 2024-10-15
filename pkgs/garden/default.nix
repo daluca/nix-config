e80f6fad2886a4886544fd377a3934120295f250
Lucas Slebos <lucas@slebos.nz>
2024-10-15 19:50:31 +1300

    update garden to version 1.9.0

--- pkgs/garden/default.nix
+++ pkgs/garden/default.nix
@@ -2,16 +2,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "garden";
-  version = "1.7.0";
+  version = "1.9.0";
 
   src = fetchFromGitLab {
     owner = "garden-rs";
     repo = "garden";
     rev = "v${version}";
-    hash = "sha256-KkvG8/WSr1HHCIoItY0hgriEilY6TzHgcs6L2GaA/lk=";
+    hash = "sha256-DtYeiCKHwF7+ZhUrAC+gIy75bzebN6FmM9WsIyrCdy4=";
   };
 
-  cargoHash = "sha256-f8UHZ/rNWio8TJj99VsO0JztxOrAgOH32zHCogtX4N8=";
+  cargoHash = "sha256-Ao+gZHFX1gKkWarqdCKyhA1PnamkVwS+3+1Wx7lnX3k=";
 
   doCheck = false;
 
