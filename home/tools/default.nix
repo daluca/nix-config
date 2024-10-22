da1679aa45fa85d97a7421ab8cbb53d5e15e86dc
Lucas Slebos <lucas@slebos.nz>
2024-10-22 19:23:23 +1300

    update tools to use inherit syntax

--- home/tools/default.nix
+++ home/tools/default.nix
@@ -1,12 +1,13 @@
 { pkgs, ... }:
-
-{
+let
+  inherit (pkgs) jq yq-go curl wl-clipboard file unzip dig;
+in {
   imports = [
     ../fzf
     ../gron
   ];
 
-  home.packages = with pkgs; [
+  home.packages = [
     jq
     yq-go
     curl
