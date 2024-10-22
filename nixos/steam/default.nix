2d594890379c0654bdc86ed7ef40e2444a891718
Lucas Slebos <lucas@slebos.nz>
2024-10-22 22:05:16 +1300

    add wayland support packages to steam

--- nixos/steam/default.nix
+++ nixos/steam/default.nix
@@ -1,12 +1,34 @@
 { lib, pkgs, ... }:
-
-{
+let
+  inherit (pkgs) ge-proton7-55 ge-proton8-32 ge-proton9-11 ge-proton9-12 ge-proton9-13 ge-proton9-14 ge-proton9-15 ge-proton9-16;
+  inherit (pkgs.xorg) libXcursor libXi libXinerama libXScrnSaver;
+  inherit (pkgs) libpng libpulseaudio libvorbis libkrb5 keyutils;
+  inherit (pkgs.stdenv.cc) cc;
+  wayland-support = [
+    # Work-around for wayland
+    # https://github.com/NixOS/nixpkgs/issues/162562#issuecomment-1229444338
+    libXcursor
+    libXi
+    libXinerama
+    libXScrnSaver
+    libpng
+    libpulseaudio
+    libvorbis
+    cc.lib
+    libkrb5
+    keyutils
+  ];
+in {
   programs.steam = {
     enable = true;
+    package = pkgs.steam.override {
+      extraLibraries = pkg: wayland-support;
+    };
     gamescopeSession.enable = true;
     remotePlay.openFirewall = true;
     localNetworkGameTransfers.openFirewall = true;
-    extraCompatPackages = with pkgs; [
+    extraCompatPackages = [
+      ge-proton9-16
       ge-proton9-15
       ge-proton9-14
       ge-proton9-13
