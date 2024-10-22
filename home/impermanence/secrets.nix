6f73a4f3bf4b0cdccf02dc5469786b0d45285b65
Lucas Slebos <lucas@slebos.nz>
2024-10-22 22:02:27 +1300

    add impermanence secrets config

--- home/impermanence/secrets.nix
+++ home/impermanence/secrets.nix
@@ -1,3 +1,8 @@
 {
   home.persistence.home.directories = [ ".config/sops/age" ];
+
+  sops.age = {
+    sshKeyPaths = [ "/persistent/system/etc/ssh/ssh_host_ed25519_key" ];
+    keyFile = "/persistent/system/var/lib/sops-nix/key.txt";
+  };
 }
