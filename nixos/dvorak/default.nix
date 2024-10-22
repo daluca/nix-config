f5b16465f22433b1c4f4a2f34eafaa54511bf298
Lucas Slebos <lucas@slebos.nz>
2024-10-22 20:57:37 +1300

    add language toggle keyboard shortcut when gnome is disabled

--- nixos/dvorak/default.nix
+++ nixos/dvorak/default.nix
@@ -1,10 +1,12 @@
-{ ... }:
-
-{
+{ config, lib, ... }:
+let
+  inherit (lib) mkIf;
+  inherit (config.services.xserver.desktopManager) gnome;
+in {
   services.xserver.xkb = {
     layout = "us,us";
     variant = ",dvorak";
-    options = "grp:win_space_toggle";
+    options = mkIf (! gnome.enable) "grp:win_space_toggle";
   };
 
   console.useXkbConfig = true;
