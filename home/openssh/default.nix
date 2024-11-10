f65536379df816e10b0c71131fc84a0318d77a57
Lucas Slebos <lucas@slebos.nz>
2024-11-10 17:15:50 +1300

    add zsh login to specific hosts

--- home/openssh/default.nix
+++ home/openssh/default.nix
@@ -1,15 +1,17 @@
-{
+let
+  zsh-login = {
+    RemoteCommand = "zsh --login";
+    RequestTTY = "force";
+  };
+in {
   programs.ssh = {
     enable = true;
     compression = true;
     hashKnownHosts = true;
-    extraOptionOverrides = {
-      RemoteCommand = "zsh --login";
-      RequestTTY = "force";
-    };
     matchBlocks = {
       gainas.hostname = "10.2.161.172";
       stormwind.hostname = "10.0.0.10";
+      stormwind.extraOptions = zsh-login;
     };
   };
 }
