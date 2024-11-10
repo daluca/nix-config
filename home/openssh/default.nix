a040ac4668b02dc63c8b0f195c0d2c5d424ade85
Lucas Slebos <lucas@slebos.nz>
2024-11-10 17:05:12 +1300

    add ssh remote zsh login command

--- home/openssh/default.nix
+++ home/openssh/default.nix
@@ -3,6 +3,10 @@
     enable = true;
     compression = true;
     hashKnownHosts = true;
+    extraOptionOverrides = {
+      RemoteCommand = "zsh --login";
+      RequestTTY = "force";
+    };
     matchBlocks = {
       gainas.hostname = "10.2.161.172";
       stormwind.hostname = "10.0.0.10";
