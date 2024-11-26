03b4e386a4a2e776c0c30f6a59d8528fd44cf8ea
Lucas Slebos <lucas@slebos.nz>
2024-11-26 15:23:41 +1300

    add workaround to get xpadneo working with the linux kernel

--- nixos/steam/default.nix
+++ nixos/steam/default.nix
@@ -1,5 +1,6 @@
 { lib, pkgs, ... }:
 let
+  inherit (lib) getName mkForce;
   inherit (pkgs) ge-proton7-55 ge-proton8-32 ge-proton9-11 ge-proton9-12 ge-proton9-13 ge-proton9-14 ge-proton9-15 ge-proton9-16 ge-proton9-17 ge-proton9-18 ge-proton9-19 ge-proton9-20;
   inherit (pkgs.xorg) libXcursor libXi libXinerama libXScrnSaver;
   inherit (pkgs) libpng libpulseaudio libvorbis libkrb5 keyutils;
@@ -47,6 +48,13 @@ in {
 
   hardware.xpadneo.enable = true;
 
+  # TODO: Update to latest kernel when underlying issue has been fixed
+  # https://github.com/NixOS/nixpkgs/issues/357693
+  # https://github.com/atar-axis/xpadneo/issues/498
+  #
+  # This is a workaround to include xpadneo in linux kernel
+  boot.kernelPackages = mkForce pkgs.unstable.linuxPackages_6_11;
+
   systemd.user.extraConfig = ''
     DefaultLimitNOFILE=${builtins.toString (8 * 1024)}
   '';
@@ -65,7 +73,7 @@ in {
     }
   ];
 
-  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
+  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (getName pkg) [
     "steam"
     "steam-unwrapped"
   ];
