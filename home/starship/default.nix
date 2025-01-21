9cf8928a12d56916c4f2dab820d222f6fb626334
Lucas Slebos <lucas@slebos.nz>
2025-01-21 16:06:33 +1300

    update starship prompt to custom format

--- home/starship/default.nix
+++ home/starship/default.nix
@@ -1,10 +1,43 @@
-{
+{ lib, ... }:
+let
+  inherit (lib) concatStringsSep;
+in {
   programs.starship = {
     enable = true;
     enableBashIntegration = false;
     settings = {
+      format = concatStringsSep "" [
+        "$directory"
+        "$fill"
+        "$all"
+        "$line_break"
+        "$character"
+      ];
+      right_format = concatStringsSep "" [
+        "$cmd_duration"
+        "$time"
+      ];
+      add_newline = true;
       direnv.disabled = false;
-      kubernetes.disabled = false;
+      fill.symbol = " ";
+      localip = {
+        disabled = false;
+        ssh_only = true;
+      };
+      nix_shell = {
+        disabled = false;
+        symbol = " ";
+        heuristic = true;
+      };
+      os = {
+        disabled = false;
+        symbols = {
+          NixOS = " ";
+        };
+      };
+      shell.disabled = false;
+      sudo.disabled = false;
+      time.disabled = false;
     };
   };
 
