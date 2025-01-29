46f70a774e1e0ed12f770a54696a6cc7ef55842b
Lucas Slebos <lucas@slebos.nz>
2025-01-29 20:39:55 +1300

    remove static ip address from ssh config

--- home/openssh/default.nix
+++ home/openssh/default.nix
@@ -9,14 +9,9 @@ in {
     compression = true;
     hashKnownHosts = true;
     matchBlocks = {
-      gainas.hostname = "10.2.161.172";
-      stormwind.hostname = "10.0.0.10";
       stormwind.extraOptions = zsh-login;
-      ironforge.hostname = "192.168.1.232";
       ironforge.extraOptions = zsh-login;
-      darnassus.hostname = "10.2.74.60";
       darnassus.extraOptions = zsh-login;
-      azeroth.hostname = "10.1.219.30";
       azeroth.extraOptions = zsh-login;
     };
   };
