7e243143e1e600e3f0e473100aeade3a85868c93
Lucas Slebos <lucas@slebos.nz>
2025-02-01 18:43:07 +1300

    add upstream dns to query router for local ip addresses

--- hosts/stormwind/adguardhome.nix
+++ hosts/stormwind/adguardhome.nix
@@ -1,6 +1,21 @@
-{
+{ config, ... }:
+let
+  hostIPAddress = "10.0.0.10";
+in {
   services.adguardhome.settings = {
-    dns.bind_hosts = [ "10.0.0.10" ];
+    dns = {
+      bind_hosts = [ hostIPAddress ];
+      upstream_dns = [
+        "[/10.in-addr.arpa/]10.0.0.1"
+        "[/internal/]10.0.0.1"
+      ];
+    };
+    filtering = {
+      rewrites = [{
+        domain = "${config.networking.hostName}.internal";
+        answer = hostIPAddress;
+      }];
+    };
     clients.persistent = [
       {
         name = "Work Laptop";
