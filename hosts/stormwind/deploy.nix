ef6b0ded193a130a1ad1f902b3da781b54511d9c
Lucas Slebos <lucas@slebos.nz>
2025-02-06 19:42:36 +1300

    update stormwind deploy to use root user

--- hosts/stormwind/deploy.nix
+++ hosts/stormwind/deploy.nix
@@ -2,7 +2,7 @@
 
 {
   hostname = "stormwind";
-  interactiveSudo = true;
+  sshUser = "root";
   profiles.system = {
     user = "root";
     path = deploy-rs.lib."aarch64-linux".activate.nixos nixosConfigurations.stormwind;
