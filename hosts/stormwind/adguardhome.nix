18f95e44894c8a6e285d31e39e0ef23d61851c72
Lucas Slebos <lucas@slebos.nz>
2025-02-25 09:56:33 +1300

    remove old work device client settings

--- hosts/stormwind/adguardhome.nix
+++ hosts/stormwind/adguardhome.nix
@@ -16,25 +16,5 @@ in {
         answer = hostIPAddress;
       }];
     };
-    clients.persistent = [
-      {
-        name = "Work Laptop";
-        ids = [ "10.0.0.40" ];
-        tags = [
-          "device_laptop"
-          "os_windows"
-        ];
-        use_global_settings = false;
-      }
-      {
-        name = "Work Phone";
-        ids = [ "10.0.0.41" ];
-        tags = [
-          "device_phone"
-          "os_android"
-        ];
-        use_global_settings = false;
-      }
-    ];
   };
 }
