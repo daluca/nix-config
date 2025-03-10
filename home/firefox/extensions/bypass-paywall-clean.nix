1d4d67f5aae5f925e77bff1546362834f78ef3ef
Lucas Slebos <lucas@slebos.nz>
2025-03-10 15:17:52 +1300

    update bypass-paywalls-clean extension

--- home/firefox/extensions/bypass-paywall-clean.nix
+++ home/firefox/extensions/bypass-paywall-clean.nix
@@ -2,9 +2,9 @@
 let
   inherit (pkgs.nur.repos.rycee) firefox-addons;
   bypass-paywalls-clean = firefox-addons.bypass-paywalls-clean.override rec {
-    version = "3.9.4.0";
+    version = "4.0.6.0";
     url = "https://gitflic.ru/project/magnolia1234/bpc_uploads/blob/raw?file=bypass_paywalls_clean-${version}.xpi";
-    sha256 = "sha256-LpeM08XTGuiNEsMnln9tW/1svjOi1OhssmMnf+Xae80=";
+    sha256 = "sha256-9OUwo31yyWs352J54xklkR2jMJu9W7r0Ghfksxp+SH8=";
   };
 in {
   programs.firefox.policies.ExtensionSettings."${bypass-paywalls-clean.addonId}" = {
