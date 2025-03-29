619a25906853dd25d487b60fa3526f81f135464b
Lucas Slebos <lucas@slebos.nz>
2025-03-30 09:07:22 +1300

    update dark theme wallpaper

--- home/desktop-managers/gnome/wallpaper/default.nix
+++ home/desktop-managers/gnome/wallpaper/default.nix
@@ -1,17 +1,16 @@
 { pkgs, ... }:
 let
-  inherit (pkgs) fetchurl;
   wallpaper = {
     color-shading-type = "solid";
     picture-options = "zoom";
-    picture-uri = "file://${fetchurl {
+    picture-uri = "file://${pkgs.fetchurl {
       url = "https://raw.githubusercontent.com/NixOS/nixos-artwork/refs/heads/master/wallpapers/nix-wallpaper-nineish.png";
       hash = "sha256-EMSD1XQLaqHs0NbLY0lS1oZ4rKznO+h9XOGDS121m9c=";
     }}";
-    picture-uri-dark = "file://${fetchurl {
-      url = "https://preview.redd.it/anime-nix-wallpaper-i-created-v0-0f6oxa9y9jlb1.png?auto=webp&s=68f30e78fad03d4add57ef3139667b402d84fdbb";
-      hash = "sha256-fEzF9L2PYf6WCKC6Q7OVx6ZIeeJnuST4gZ45QEMPpmY=";
-      name = "nix-wallpaper-nineish-anime.png";
+    picture-uri-dark = "file://${pkgs.fetchurl {
+      url = "https://i.imgur.com/xsSEqjJ.jpeg";
+      hash = "sha256-7AcgbUEOfcdFHl2aBGw8WdPnTpwG2GaKQRTFU1bNsS0=";
+      name = "nighty-mode-wallpaper.jpeg";
     }}";
     primary-color = "#000000000000";
     secondary-color = "#000000000000";
