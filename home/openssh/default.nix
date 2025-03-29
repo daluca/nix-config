777c61e88913479fd5ed5f91c2ba93be462c4ce3
Lucas Slebos <lucas@slebos.nz>
2025-03-30 10:31:25 +1300

    add unifi ip address to exclude ssh zsh login

--- home/openssh/default.nix
+++ home/openssh/default.nix
@@ -1,4 +1,4 @@
-{ config, ... }:
+{ config, lib, ... }:
 
 {
   programs.ssh = {
@@ -6,10 +6,21 @@
     compression = true;
     hashKnownHosts = true;
     matchBlocks."${config.home.username}" = {
-      match = "User ${config.home.username} Host *,!gainas";
+      match = "User ${config.home.username} Host *,!${lib.concatStringsSep ",!" [
+        "192.168.1.1"
+        "192.168.1.2"
+        "192.168.1.3"
+        "192.168.1.4"
+      ]}";
       extraOptions = {
         RemoteCommand = "zsh --login";
         RequestTTY = "yes";
+      };
+    };
+    matchBlocks.usg = {
+      host = "192.168.1.1";
+      extraOptions = {
+        PubkeyAcceptedKeyTypes = "+ssh-rsa";
       };
     };
   };
