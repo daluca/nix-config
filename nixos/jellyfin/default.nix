7287ec2e64acc985b595c235a53732773fcf4f84
Lucas Slebos <lucas@slebos.nz>
2025-04-28 21:05:27 +0200

    add recommended jellyfin nginx configuration

--- nixos/jellyfin/default.nix
+++ nixos/jellyfin/default.nix
@@ -13,8 +13,25 @@ in {
       sslCertificate = "${cert.directory}/fullchain.pem";
       sslCertificateKey = "${cert.directory}/key.pem";
       sslTrustedCertificate = "${cert.directory}/chain.pem";
-      locations."/" = {
-        proxyPass = "http://127.0.0.1:8096";
+      extraConfig = /* nginx */ ''
+        client_max_body_size 20M;
+        add_header X-Content-Type-Options "nosniff";
+        add_header Permissions-Policy "accelerometer=(), ambient-light-sensor=(), battery=(), bluetooth=(), camera=(), clipboard-read=(), display-capture=(), document-domain=(), encrypted-media=(), gamepad=(), geolocation=(), gyroscope=(), hid=(), idle-detection=(), interest-cohort=(), keyboard-map=(), local-fonts=(), magnetometer=(), microphone=(), payment=(), publickey-credentials-get=(), serial=(), sync-xhr=(), usb=(), xr-spatial-tracking=()" always;
+        add_header Content-Security-Policy "default-src https: data: blob: ; img-src 'self' https://* ; style-src 'self' 'unsafe-inline'; script-src 'self' 'unsafe-inline' https://www.gstatic.com https://www.youtube.com blob:; worker-src 'self' blob:; connect-src 'self'; object-src 'none'; frame-ancestors 'self'; font-src 'self'";
+      '';
+      locations = let
+        jellyfin = "http://127.0.0.1:8096";
+      in {
+        "/" = {
+          proxyPass = jellyfin;
+          extraConfig = /* nginx */ ''
+            proxy_buffering off;
+          '';
+        };
+        "/socket" = {
+          proxyPass = jellyfin;
+          proxyWebsockets = true;
+        };
       };
     };
   };
