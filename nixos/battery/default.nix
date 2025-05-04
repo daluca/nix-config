c79e6d500202ec558d4491a4e5eb567b7948b95e
Lucas Slebos <lucas@slebos.nz>
2025-05-04 17:52:38 +0200

    update battery threshold limit to 81

--- nixos/battery/default.nix
+++ nixos/battery/default.nix
@@ -6,7 +6,7 @@
     after = [ "multi-user.target" "suspend.target" "hibernate.target" "hybrid-sleep.target" "suspend-then-hibernate.target" ];
 
     script = /* bash */ ''
-      ${pkgs.coreutils}/bin/echo 80 > /sys/class/power_supply/BAT0/charge_control_end_threshold
+      ${pkgs.coreutils}/bin/echo 81 > /sys/class/power_supply/BAT0/charge_control_end_threshold
     '';
     serviceConfig = {
       Type = "oneshot";
