e06f67c4f32ea4ae84bb47901480bc58bf3931d1
Lucas Slebos <lucas@slebos.nz>
2025-05-11 20:38:32 +0200

    update firefox layout

--- home/firefox/layout.nix
+++ home/firefox/layout.nix
@@ -1,18 +1,29 @@
 builtins.toJSON {
   placements = {
     widget-overflow-fixed-list = [];
+    unified-extensions-area = [
+      "ublock0_raymondhill_net-browser-action" # uBlock Origin
+      "addon_simplelogin-browser-action" # SimpleLogin
+      "magnolia_12_34-browser-action" # Bypass Paywalls Clean
+      "jid1-bofifl9vbdl2zq_jetpack-browser-action" # Decentraleyes
+      "_74145f27-f039-47ce-a470-a662b129930a_-browser-action" # Clear URLs
+      "_7a7a4a92-a2a0-41d1-9fd7-1e92480d612d_-browser-action" # Stylus
+      "sponsorblocker_ajay_app-browser-action" # SponsorBlock
+      "gdpr_cavi_au_dk-browser-action" # Consent-O-Matic
+    ];
     nav-bar = [
       "sidebar-button"
       "back-button"
       "forward-button"
       "stop-reload-button"
+      "vertical-spacer"
       "customizableui-special-spring1"
       "urlbar-container"
-      "customizableui-special-spring2"
-      "save-to-pocket-button"
       "downloads-button"
+      "customizableui-special-spring2"
       "jordanlinkwarden_gmail_com-browser-action" # Linkwarden
       "fxa-toolbar-menu-button"
+      "_446900e4-71c2-419f-a6a7-df9c091e268b_-browser-action" # Bitwarden
       "unified-extensions-button"
       "_testpilot-containers-browser-action" # Firefox Multi-Account Containers
       "firefox-view-button"
@@ -29,16 +40,6 @@ builtins.toJSON {
     PersonalToolbar = [
       "personal-bookmarks"
     ];
-    unified-extensions-area = [
-      "ublock0_raymondhill_net-browser-action" # uBlock Origin
-      "addon_simplelogin-browser-action" # SimpleLogin
-      "magnolia_12_34-browser-action" # Bypass Paywalls Clean
-      "jid1-bofifl9vbdl2zq_jetpack-browser-action" # Decentraleyes
-      "_74145f27-f039-47ce-a470-a662b129930a_-browser-action" # Clear URLs
-      "_446900e4-71c2-419f-a6a7-df9c091e268b_-browser-action" # Bitwarden
-      "_7a7a4a92-a2a0-41d1-9fd7-1e92480d612d_-browser-action" # Stylus
-      "sponsorblocker_ajay_app-browser-action" # SponsorBlock
-    ];
   };
   seen = [
     "jordanlinkwarden_gmail_com-browser-action" # Linkwarden
@@ -52,13 +53,16 @@ builtins.toJSON {
     "addon_simplelogin-browser-action" # SimpleLogin
     "sponsorblocker_ajay_app-browser-action" # SponsorBlock
     "developer-button"
+    "gdpr_cavi_au_dk-browser-action" # Consent-O-Matic
   ];
   dirtyAreaCache = [
     "unified-extensions-area"
     "TabsToolbar"
     "nav-bar"
     "vertical-tabs"
+    "toolbar-menubar"
+    "PersonalToolbar"
   ];
-  currentVersion = 20;
-  newElementCount = 7;
+  currentVersion = 22;
+  newElementCount = 8;
 }
