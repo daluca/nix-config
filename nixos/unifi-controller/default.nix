0e725da585ebc71fbdf2af709fe6ea97c122720f
Lucas Slebos <lucas@slebos.nz>
2025-05-24 16:47:26 +0200

    remove unifi package override

--- nixos/unifi-controller/default.nix
+++ nixos/unifi-controller/default.nix
@@ -3,15 +3,6 @@
 {
   services.unifi = {
     enable = true;
-    # TODO: Remove package override in NixOS 25.05
-    unifiPackage = pkgs.unifi.overrideAttrs (oldAttrs: rec {
-      version = "9.0.114";
-
-      src = pkgs.fetchurl {
-        url = "https://dl.ubnt.com/unifi/${version}/unifi_sysvinit_all.deb";
-        sha256 = "sha256-3xumIIzr+tx60kPhPfSs2Kz2iJ39Kt5934Vca/MpUu4=";
-      };
-    });
     mongodbPackage = pkgs.mongodb-ce;
     openFirewall = true;
   };
