0f9eef9457dc101d283fbca1bb3201d57470d21b
Lucas Slebos <lucas@slebos.nz>
2025-05-30 21:58:09 +0200

    add wasistlos settings ini file

--- home/whatsapp/default.nix
+++ home/whatsapp/default.nix
@@ -1,9 +1,23 @@
-{ pkgs, ... }:
+{ lib, pkgs, ... }:
 
 {
   home.packages = with pkgs; [
     wasistlos
   ];
+
+  xdg.configFile."wasistlos/settings.conf".text = lib.generators.toINI {} {
+    general = {
+      notification-sounds = true;
+      close-to-tray = true;
+    };
+    web = {
+      hw-accel = 1;
+      allow-permissions = true;
+    };
+    appearance = {
+      prefer-dark-theme = true;
+    };
+  };
 
   home.persistence.home.directories = [
     ".local/share/wasistlos"
