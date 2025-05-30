228198a58b44aa124b5e2fece5246068223d5e94
Lucas Slebos <lucas@slebos.nz>
2025-05-30 20:30:26 +0200

    add tools to modern-unix

--- home/modern-unix/default.nix
+++ home/modern-unix/default.nix
@@ -1,8 +1,18 @@
+{ pkgs, ... }:
+
 {
   imports = [
     ./bat.nix
     ./eza.nix
     ./fd.nix
     ./ripgrep.nix
+  ];
+
+  home.packages = with pkgs.unstable; [
+    duf
+    dust
+    mprocs
+    procs
+    xh
   ];
 }
