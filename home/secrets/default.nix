5613171148380269f7b526adc3ccfcb79d84b817
Lucas Slebos <lucas@slebos.nz>
2025-06-04 10:19:17 +0200

    remove sops-nix module from shared modules and import on demand

--- home/secrets/default.nix
+++ home/secrets/default.nix
@@ -1,6 +1,10 @@
-{ config, lib, ... }:
+{ config, lib, inputs, ... }:
 
 {
+  imports = [
+    inputs.sops-nix.homeManagerModules.default
+  ];
+
   sops = {
     defaultSopsFile = lib.custom.relativeToRoot "secrets/secrets.sops.yaml";
     age.keyFile = "${config.xdg.configHome}/sops/age/nix-config.txt";
--- nixos/secrets/sops.nix
+++ nixos/secrets/sops.nix
@@ -5,10 +5,6 @@
     inputs.sops-nix.nixosModules.sops
   ];
 
-  home-manager.sharedModules = [
-    inputs.sops-nix.homeManagerModules.sops
-  ];
-
   sops = {
     defaultSopsFile = ../../secrets/secrets.sops.yaml;
     age = {
