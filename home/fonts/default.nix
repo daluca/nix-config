ac39ad4c36e53a56bfd9872e93364e7fbc33c8d5
Lucas Slebos <lucas@slebos.nz>
2025-06-04 14:06:54 +0200

    update fonts package in home modules

--- home/fonts/default.nix
+++ home/fonts/default.nix
@@ -3,7 +3,8 @@
 {
   fonts.fontconfig.enable = true;
 
-  home.packages = with pkgs; [
-    (nerdfonts.override { fonts = [ "Meslo" "FiraCode" ]; })
+  home.packages = with pkgs.nerd-fonts; [
+    meslo-lg
+    fira-code
   ];
 }
