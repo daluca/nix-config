d7d4ba9d5f14e0035ce5e3691a319c9ace484300
Lucas Slebos <lucas@slebos.nz>
2025-06-17 16:51:14 +0200

    update ssh daemon to prohibit root password

--- nixos/openssh/server/default.nix
+++ nixos/openssh/server/default.nix
@@ -4,6 +4,7 @@
     startWhenNeeded = true;
     ports = [ 22 ];
     settings = {
+      PermitRootLogin = "prohibit-password";
       PasswordAuthentication = false;
       X11Forwarding = false;
     };
