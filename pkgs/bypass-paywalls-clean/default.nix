8b851adb6b59af0bcbd056d59d43daaa0a16319b
Lucas Slebos <lucas@slebos.nz>
2025-07-05 16:52:40 +0200

    update flake lock

--- pkgs/bypass-paywalls-clean/default.nix
+++ pkgs/bypass-paywalls-clean/default.nix
@@ -2,11 +2,11 @@
 
 buildFirefoxXpiAddon rec {
   pname = "bypass-paywalls-clean";
-  version = "4.1.1.3";
+  version = "4.1.5.0";
 
   addonId = "magnolia@12.34";
   url = "https://gitflic.ru/project/magnolia1234/bpc_uploads/blob/raw?file=bypass_paywalls_clean-${version}.xpi";
-  sha256 = "sha256-hRyiZ+pYnzgSLPMu+YE7jKcuTEVsEgNQhAUpP7AYRHA=";
+  sha256 = "sha256-6ildFYDKmQMbUf4Q4jxiCm/D5Cjyna0UPD1oOIfT5Pc=";
 
   meta = with lib; {
     homepage = "https://twitter.com/Magnolia1234B";
