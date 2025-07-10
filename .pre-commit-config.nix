195e44480db32d90d3712b3e8976489c522fa31e
Lucas Slebos <lucas@slebos.nz>
2025-07-10 16:03:03 +0200

    update custom pre-commit hook names

--- .pre-commit-config.nix
+++ .pre-commit-config.nix
@@ -16,24 +16,21 @@ rec {
   };
   markdownlint-cli2 = {
     enable = true;
-    name = "markdownlint-cli2";
-    description = "Checks the style of Markdown/CommonMark files.";
+    description = "markdownlint-cli2 hook";
     package = pkgs.markdownlint-cli2;
     entry = "${markdownlint-cli2.package}/bin/markdownlint-cli2";
     types = [ "markdown" ];
   };
   gitleaks = {
     enable = true;
-    name = "Detect hardcoded secrets";
-    description = "Detect hardcoded secrets using Gitleaks";
+    description = "gitleaks hook";
     package = pkgs.gitleaks;
     entry = "${gitleaks.package}/bin/gitleaks protect --verbose --redact --staged";
     pass_filenames = false;
   };
   commitlint-rs = {
     enable = false;
-    name = "Assert Conventional Commit Messages";
-    description = "Asserts that Conventional Commits have been used for all commit messages according to the rules for this repo.";
+    description = "commitlint-rs hook";
     package = pkgs.commitlint-rs;
     entry = "${commitlint-rs.package}/bin/commitlint --edit";
     stages = [ "prepare-commit-msg" ];
