7cd3d83054ef0ee8e5afba2d34b8e70162513a6f
Lucas Slebos <lucas@slebos.nz>
2025-08-08 13:48:18 +0200

    add readline for partial history searching

--- home/bash/default.nix
+++ home/bash/default.nix
@@ -1,3 +1,11 @@
 {
   programs.bash.enable = true;
+
+  programs.readline = {
+    enable = true;
+    bindings = {
+      "\\e[A" = "history-search-backward";
+      "\\e[B" = "history-search-forward";
+    };
+  };
 }
