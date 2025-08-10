430968505f021b286084c35c2241b9596ac79574
Lucas Slebos <lucas@slebos.nz>
2025-08-10 18:50:21 +0200

    add unifi to ssh known hosts

--- nixos/openssh/default.nix
+++ nixos/openssh/default.nix
@@ -1,22 +1,20 @@
-{ config, lib, ... }:
+{ config, lib, secrets, ... }:
 let
-  inherit (lib.path) append;
   inherit (config.networking) hostName domain;
-  inherit (config.sops) secrets;
 in {
-  environment.etc."ssh/ssh_host_ed25519_key.pub".source = append ../../hosts "${hostName}/keys/ssh_host_ed25519_key.pub";
+  environment.etc."ssh/ssh_host_ed25519_key.pub".source = lib.custom.relativeToHosts "${hostName}/keys/ssh_host_ed25519_key.pub";
 
   environment.etc."ssh/ssh_host_ed25519_key" = {
-    source = secrets."ssh_host_ed25519_key".path;
+    source = config.sops.secrets."ssh_host_ed25519_key".path;
     mode = "0400";
   };
 
   sops.secrets."ssh_host_ed25519_key".key = "id_ed25519";
 
-  environment.etc."ssh/ssh_host_rsa_key.pub".source = append ../../hosts "${hostName}/keys/ssh_host_rsa_key.pub";
+  environment.etc."ssh/ssh_host_rsa_key.pub".source = lib.custom.relativeToHosts "${hostName}/keys/ssh_host_rsa_key.pub";
 
   environment.etc."ssh/ssh_host_rsa_key" = {
-    source = secrets."ssh_host_rsa_key".path;
+    source = config.sops.secrets."ssh_host_rsa_key".path;
     mode = "0400";
   };
 
@@ -62,6 +60,14 @@ in {
     "guiltyspark/rsa" = {
       hostNames = [ "guiltyspark" ] ++ guiltyspark.extraHostNames;
       publicKeyFile = ../../hosts/guiltyspark/keys/ssh_host_rsa_key.pub;
+    };
+    unifi = {
+      extraHostNames = [ "unifi.${domain}" secrets.hosts.unifi.ipv4-address ];
+      publicKeyFile = ../../hosts/unifi/keys/ssh_host_ed25519_key.pub;
+    };
+    "unifi/rsa" = {
+      hostNames = [ "unifi" ] ++ unifi.extraHostNames;
+      publicKeyFile = ../../hosts/unifi/keys/ssh_host_rsa_key.pub;
     };
     "github.com".publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl";
     "github.com/rsa".publicKey = "github.com ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCj7ndNxQowgcQnjshcLrqPEiiphnt+VTTvDP6mHBL9j1aNUkY4Ue1gvwnGLVlOhGeYrnZaMgRK6+PKCUXaDbC7qtbW8gIkhL7aGCsOr/C56SJMy/BCZfxd1nWzAOxSDPgVsmerOBYfNqltV9/hWCqBywINIR+5dIg6JTJ72pcEpEjcYgXkE2YEFXV1JHnsKgbLWNlhScqb2UmyRkQyytRLtL+38TGxkxCflmO+5Z8CSSNY7GidjMIZ7Q4zMjA2n1nGrlTDkzwDCsw+wqFPGQA179cnfGWOWRVruj16z6XyvxvjJwbz0wQZ75XK5tKSb7FNyeIEs4TT4jk+S4dhPeAUC5y+bDYirYgM4GC7uEnztnZyaVWQ7B381AK4Qdrwt51ZqExKbQpTUNn+EjqoTwvqNj4kqx5QUCI0ThS/YkOxJCXmPUWZbhjpCg56i+2aB6CmK2JGhn57K5mj0MNdBXA4/WnwH6XoPWJzK5Nyu2zB3nAZp+S5hpQs+p1vN1/wsjk=";
