aa2d66c0e740ca14d872330a3c9b79f09829c38c
Lucas Slebos <lucas@slebos.nz>
2025-08-12 21:50:21 +0200

    add persistent gpg key path

--- nixos/impermanence/secrets.nix
+++ nixos/impermanence/secrets.nix
@@ -1,10 +1,13 @@
 { lib, ... }:
 
 {
-  sops.age = {
-    keyFile = lib.mkForce "/persistent/system/var/lib/sops-nix/keys.txt";
-    sshKeyPaths = lib.mkForce [
+  sops = {
+    age.keyFile = lib.mkForce "/persistent/system/var/lib/sops-nix/keys.txt";
+    age.sshKeyPaths = [
       "/persistent/system/etc/ssh/ssh_host_ed25519_key"
+    ];
+    gnupg.sshKeyPaths = [
+      "/persistent/system/etc/ssh/ssh_host_rsa_key"
     ];
   };
 }
