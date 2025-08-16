ce8a86b12f6e1b2f1db42b9137eea37c7880bf10
Lucas Slebos <lucas@slebos.nz>
2025-08-16 20:17:52 +0200

    add battery starting threshold

--- nixos/battery/default.nix
+++ nixos/battery/default.nix
@@ -1,16 +1,13 @@
-{ pkgs, ... }:
-
 {
   systemd.services."battery-threshold" = {
     description = "Set battery charging threshold";
     after = [ "multi-user.target" "suspend.target" "hibernate.target" "hybrid-sleep.target" "suspend-then-hibernate.target" ];
 
     script = /* bash */ ''
-      ${pkgs.coreutils}/bin/echo 81 > /sys/class/power_supply/BAT0/charge_control_end_threshold
+      echo 21 > /sys/class/power_supply/BAT0/charge_control_start_threshold
+      echo 81 > /sys/class/power_supply/BAT0/charge_control_end_threshold
     '';
-    serviceConfig = {
-      Type = "oneshot";
-    };
+    serviceConfig.Type = "oneshot";
 
     wantedBy = [ "multi-user.target" "suspend.target" "hibernate.target" "hybrid-sleep.target" "suspend-then-hibernate.target" ];
   };
