e742213f5889c8a5920a8ec0e6c78a9dc07c7b77
Lucas Slebos <lucas@slebos.nz>
2025-08-17 15:50:07 +0200

    remove accept-dns flag from tailscale

--- nixos/tailscale/common.nix
+++ nixos/tailscale/common.nix
@@ -1,18 +1,16 @@
 { config, pkgs, secrets, ... }:
 let
-  inherit (pkgs.unstable) tailscale;
   inherit (config.home-manager.users) daluca;
 in {
   services.tailscale = {
     enable = true;
-    package = tailscale;
+    package = pkgs.unstable.tailscale;
     openFirewall = true;
     authKeyFile = config.sops.secrets."tailscale/preauthkey".path;
     extraUpFlags = [
       "--login-server=https://headscale.${secrets.cloud.domain}"
       "--operator=${daluca.home.username}"
       "--accept-routes"
-      "--accept-dns"
       "--reset"
     ];
   };
