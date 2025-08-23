ba4b7d53e5fdcc6ee29c95e06235f8d7784c3e64
Lucas Slebos <lucas@slebos.nz>
2025-08-23 19:30:09 +0200

    update bypass-paywalls-clean package

--- pkgs/bypass-paywalls-clean/default.nix
+++ pkgs/bypass-paywalls-clean/default.nix
@@ -2,11 +2,11 @@
 
 buildFirefoxXpiAddon rec {
   pname = "bypass-paywalls-clean";
-  version = "4.1.7.5";
+  version = "4.1.8.3";
 
   addonId = "magnolia@12.34";
   url = "https://gitflic.ru/project/magnolia1234/bpc_uploads/blob/raw?file=bypass_paywalls_clean-${version}.xpi";
-  sha256 = "sha256-RnSV0Afv9CsOKcQ7psUswvHVYRZq3txNBXrFDTc5Inc=";
+  sha256 = "sha256-OF2IyrsDahLdee3rqR0ddL2nJaP3tTAkanSzTLc0tKw=";
 
   meta = with lib; {
     homepage = "https://twitter.com/Magnolia1234B";
