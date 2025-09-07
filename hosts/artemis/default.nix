76f300ddf53515d81356030f00bef4ead73694e9
Lucas Slebos <lucas@slebos.nz>
2025-09-07 22:33:58 +0200

    add nixos-raspberrypi cachix substituter

--- hosts/artemis/default.nix
+++ hosts/artemis/default.nix
@@ -34,7 +34,11 @@
 
   time.timeZone = "Europe/Amsterdam";
 
-  nix.settings.warn-dirty = false;
+  nix.settings = {
+    warn-dirty = false;
+    substituters = [ "https://nixos-raspberrypi.cachix.org" ];
+    trusted-public-keys = [ "nixos-raspberrypi.cachix.org-1:4iMO9LXa8BqhU+Rpg6LQKiGa2lsNh/j2oiYLNOQ5sPI=" ];
+  };
 
   host.battery = true;
 
