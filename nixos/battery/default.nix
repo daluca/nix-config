3d69a8d8f6475d6c9574c4d83fcd81bb0c192e4b
Lucas Slebos <lucas@slebos.nz>
2025-11-15 21:17:38 +0100

    update battery start threshold to 31 percent

--- nixos/battery/default.nix
+++ nixos/battery/default.nix
@@ -4,7 +4,7 @@
     after = [ "multi-user.target" "suspend.target" "hibernate.target" "hybrid-sleep.target" "suspend-then-hibernate.target" ];
 
     script = /* bash */ ''
-      echo 21 > /sys/class/power_supply/BAT0/charge_control_start_threshold
+      echo 31 > /sys/class/power_supply/BAT0/charge_control_start_threshold
       echo 81 > /sys/class/power_supply/BAT0/charge_control_end_threshold
     '';
     serviceConfig.Type = "oneshot";
