95dec61d30c82ec6a914fe82405a565eacdd0bd2
Lucas Slebos <lucas@slebos.nz>
2025-11-30 14:13:05 +0100

    update tailscale common to default

--- nixos/tailscale/default.nix
+++ nixos/tailscale/default.nix
@@ -1,5 +1,21 @@
+{ config, secrets, ... }:
+
 {
-  imports = [
-    ./client
-  ];
+  services.tailscale = {
+    enable = true;
+    openFirewall = true;
+    authKeyFile = config.sops.secrets."tailscale/preauthkey".path;
+    extraUpFlags = [
+      "--login-server=https://headscale.${secrets.cloud.domain}"
+      "--operator=${config.home-manager.users.daluca.home.username}"
+      "--accept-routes"
+      "--reset"
+    ];
+  };
+
+  sops.secrets."tailscale/preauthkey" = {
+    owner = config.home-manager.users.daluca.home.username;
+  };
+
+  networking.firewall.trustedInterfaces = [ "tailscaled0" ];
 }
--- nixos/tailscale/server/default.nix
+++ nixos/tailscale/server/default.nix
@@ -1,6 +1,6 @@
 {
   imports = [
-    ../common.nix
+    ./..
   ];
 
   services.tailscale = {
