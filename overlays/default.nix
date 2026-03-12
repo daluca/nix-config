a242a398d5d9811c47ca21e2f8d71dcddeb54913
Lucas Slebos <lucas@slebos.nz>
2026-03-12 20:20:46 +0100

    remove openthread-border-router overlay

--- overlays/default.nix
+++ overlays/default.nix
@@ -11,8 +11,6 @@
 
     fzf-preview = fzf-preview.packages.${system}.fzf-preview;
 
-    openthread-border-router = openthread-border-router.legacyPackages.${system}.openthread-border-router;
-
     helium = nur.legacyPackages.${system}.repos.Ev357.helium;
 
     itch = let
