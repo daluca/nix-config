fca3b86d87c3a2fee6abd41dcd57a70d60eb691c
Lucas Slebos <lucas@slebos.nz>
2026-03-21 16:08:56 +0100

    update charlie with remote-unlocking module

--- hosts/charlie/default.nix
+++ hosts/charlie/default.nix
@@ -1,9 +1,7 @@
-{ lib, inputs, ... }:
+{ lib, ... }:
 
 {
-  imports = with inputs; [
-    disko.nixosModules.disko
-  ] ++ [
+  imports = [
     ./..
     ./disko.nix
   ] ++ map (m: lib.custom.relativeToRoot m) [
@@ -11,32 +9,11 @@
   ] ++ map (m: lib.custom.relativeToUsers m) [
     "remotebuild"
   ] ++ map (m: lib.custom.relativeToNixosModules m) [
-    "impermanence"
+    "impermanence/grub"
+    "remote-unlocking/dhcp"
   ];
 
   networking.hostName = "charlie";
-
-  boot = {
-    kernelParams = [ "ip=dhcp" ];
-    initrd = {
-      availableKernelModules = [ "virtio-pci" ];
-      network = {
-        enable = true;
-        ssh = {
-          enable = true;
-          port = 22;
-          authorizedKeyFiles = [
-            (lib.custom.relativeToUsers "daluca/keys/id_ed25519.pub")
-          ];
-          hostKeys = [
-            "/persistent/system/etc/ssh/ssh_host_ed25519_key"
-            "/persistent/system/etc/ssh/ssh_host_rsa_key"
-          ];
-          shell = "/bin/cryptsetup-askpass";
-        };
-      };
-    };
-  };
 
   system.stateVersion = "25.11";
 }
--- hosts/charlie/openssh.nix
+++ hosts/charlie/openssh.nix
@@ -10,5 +10,14 @@
       hostNames = [ "charlie" ] ++ charlie.extraHostNames;
       publicKeyFile = ./keys/ssh_host_rsa_key.pub;
     };
+    charlie-initrd = {
+      hostNames = [ "[charlie]:22022" ] ++ charlie-initrd.extraHostNames;
+      extraHostNames = [ "[charlie.${config.networking.domain}]:22022" "[${secrets.hosts.charlie.ipv4-address}]:22022" ];
+      publicKeyFile = ./keys/ssh_initrd_ed25519_key.pub;
+    };
+    "charlie-initrd/rsa" = {
+      hostNames = [ "[charlie]:22022" ] ++ charlie-initrd.extraHostNames;
+      publicKeyFile = ./keys/ssh_initrd_rsa_key.pub;
+    };
   };
 }
