045258db53206a00878f9f8c7f80856ae4273e57
Lucas Slebos <lucas@slebos.nz>
2026-05-16 13:22:18 +0200

    add jjui edit from details plugin

--- home/jujutsu/default.nix
+++ home/jujutsu/default.nix
@@ -143,6 +143,30 @@
         show_at_start = true;
       };
     };
+    configLua = /* lua */ ''
+      function setup(config)
+        config.action("edit file", function()
+          local function first_hunk_new_lineno(git_diff)
+            for line in git_diff:gmatch("[^\n]+") do
+              if line:sub(1, 3) == "@@ " then
+                local new_start = line:match("%+(%d+)")
+                if new_start then
+                  return tonumber(new_start)
+                end
+              end
+            end
+            return nil
+          end
+
+          local diff = jj("diff", "--git", "-r", context.change_id(), context.file())
+          local line_number = first_hunk_new_lineno(diff)
+          exec_shell(string.format("nvim +%q %q", line_number, context.file()))
+        end, {
+          scope = "revisions.details",
+          key = "e",
+        })
+      end
+    '';
   };
 
   home.persistence.home.directories = [
