549e1d4e501f4b4455f175e431a3d0e8db128cd3
Lucas Slebos <lucas@slebos.nz>
2026-05-16 13:22:18 +0200

    update nextflux package

--- pkgs/nextflux/default.nix
+++ pkgs/nextflux/default.nix
@@ -6,13 +6,13 @@
 
 buildNpmPackage {
   pname = "nextflux";
-  version = "unstable-2026-05-15";
+  version = "unstable-2026-05-20";
 
   src = fetchFromGitHub {
     owner = "electh";
     repo = "nextflux";
     rev = "main";
-    hash = "sha256-aGhMk13xbJgYDelPcb3lwViNGjAc+tl1SzTAN8lB9HI=";
+    hash = "sha256-6ReEqo8ZVgote5ysXAjBEIk6IzNTLK7f2Mx+JGT0sEU=";
   };
 
   npmDepsHash = "sha256-yF/pSY0em/BMqUrBjjtcFgF0dtgsZWAUg6BDXAQOyZU=";
