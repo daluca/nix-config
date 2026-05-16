401c7ea94bc1d8d01f8857815c13485ae8bcb5e5
Lucas Slebos <lucas@slebos.nz>
2026-05-16 13:22:18 +0200

    update whatsapp module with karere package

--- home/whatsapp/default.nix
+++ home/whatsapp/default.nix
@@ -1,25 +1,11 @@
-{ lib, pkgs, ... }:
+{ pkgs, ... }:
 
 {
   home.packages = with pkgs; [
-    wasistlos
+    karere
   ];
 
-  xdg.configFile."wasistlos/settings.conf".text = lib.generators.toINI { } {
-    general = {
-      notification-sounds = true;
-      close-to-tray = true;
-    };
-    web = {
-      hw-accel = 1;
-      allow-permissions = true;
-    };
-    appearance = {
-      prefer-dark-theme = true;
-    };
-  };
-
   home.persistence.home.directories = [
-    ".local/share/wasistlos"
+    ".local/share/karere"
   ];
 }
