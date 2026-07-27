3ef221d455aa646ef184441056b77b0144be1b77
Lucas Slebos <lucas@slebos.nz>
2026-07-27 18:29:30 +0200

    add meta module for flake-parts homeManagerModules option

---
+++ modules/dendritic/meta.nix
@@ -0,0 +1,26 @@
+{
+  lib,
+  flake-parts-lib,
+  moduleLocation,
+  ...
+}:
+
+with lib; {
+  options = {
+    flake = flake-parts-lib.mkSubmoduleOptions {
+      homeManagerModules = mkOption {
+        type = types.lazyAttrsOf types.deferredModule;
+        default = { };
+        apply = mapAttrs (k: v: {
+          _file = "${toString moduleLocation}#homeManagerModules.${k}";
+          imports = [v];
+        });
+        description = ''
+          Home Manager modules.
+
+          You may use this for reusable pieces of configuration, service modules, etc.
+        '';
+      };
+    };
+  };
+}
