14c6dbc198934c489a9e89fd4a1ff0f2a6ea7ba2
Lucas Slebos <lucas@slebos.nz>
2026-08-08 09:51:26 +0200

    update steam to be a module

---
+++ modules/steam.nix
@@ -0,0 +1,72 @@
+{ self, ... }:
+
+{
+  flake.nixosModules.steam = { pkgs, ... }: {
+    programs.steam = {
+      enable = true;
+      package = pkgs.steam.override {
+        extraProfile = ''
+          export SDL_JOYSTICK_HIDAPI_XBOX=0
+        '';
+
+        extraLibraries =
+          _pkg: with pkgs; [
+            # Work-around for wayland
+            # https://github.com/NixOS/nixpkgs/issues/162562#issuecomment-1229444338
+            libxcursor
+            libxi
+            libxinerama
+            libxscrnsaver
+            libpng
+            libpulseaudio
+            libvorbis
+            stdenv.cc.cc.lib
+            libkrb5
+            keyutils
+          ];
+      };
+      gamescopeSession.enable = true;
+      remotePlay.openFirewall = true;
+      localNetworkGameTransfers.openFirewall = true;
+      extraCompatPackages = with pkgs; [
+        GE-Proton11-1
+        GE-Proton10-34
+        GE-Proton10-33
+        GE-Proton10-32
+        GE-Proton10-31
+        GE-Proton10-30
+        GE-Proton9
+        GE-Proton8
+        GE-Proton7
+      ];
+    };
+
+    programs.gamemode.enable = true;
+
+    hardware.xpadneo.enable = true;
+
+    home-manager.users.daluca.imports = with self.homeManagerModules; [
+      steam
+    ];
+  };
+
+  flake.homeManagerModules.steam = { pkgs, ... }: {
+    xdg.dataFile = with pkgs; {
+      "Steam/compatibilitytools.d/GE-Proton11".source = lib.getOutput "steamcompattool" GE-Proton11;
+      "Steam/compatibilitytools.d/GE-Proton11-1".source = lib.getOutput "steamcompattool" GE-Proton11-1;
+      "Steam/compatibilitytools.d/GE-Proton10".source = lib.getOutput "steamcompattool" GE-Proton10;
+      "Steam/compatibilitytools.d/GE-Proton10-34".source = lib.getOutput "steamcompattool" GE-Proton10-34;
+      "Steam/compatibilitytools.d/GE-Proton10-33".source = lib.getOutput "steamcompattool" GE-Proton10-33;
+      "Steam/compatibilitytools.d/GE-Proton10-32".source = lib.getOutput "steamcompattool" GE-Proton10-32;
+      "Steam/compatibilitytools.d/GE-Proton10-31".source = lib.getOutput "steamcompattool" GE-Proton10-31;
+      "Steam/compatibilitytools.d/GE-Proton10-30".source = lib.getOutput "steamcompattool" GE-Proton10-30;
+      "Steam/compatibilitytools.d/GE-Proton9".source = lib.getOutput "steamcompattool" GE-Proton9;
+      "Steam/compatibilitytools.d/GE-Proton8".source = lib.getOutput "steamcompattool" GE-Proton8;
+      "Steam/compatibilitytools.d/GE-Proton7".source = lib.getOutput "steamcompattool" GE-Proton7;
+    };
+
+    home.persistence.home.directories = [
+      ".local/share/Steam"
+    ];
+  };
+}
