0985124d27d0d244386155e734f2918cf9298180
Lucas Slebos <lucas@slebos.nz>
2026-08-08 09:51:26 +0200

    add host specific override to daluca user on benedick host

--- modules/users/daluca/benedick.nix
+++ modules/users/daluca/benedick.nix
@@ -1,7 +1,7 @@
 { self, ... }:
 
 {
-  flake.homeManagerModules.users-daluca-benedick = {
+  flake.homeManagerModules.users-daluca-benedick = { config, lib, pkgs, ... }: {
     imports = with self.homeManagerModules; [
       users-daluca
 
@@ -20,5 +20,17 @@
     ];
 
     programs.custom-firefox.default = "zen-browser";
+
+    programs.tmux.extraConfig = /* tmux */ ''
+      bind C-j display-popup -d "#{pane_current_path}" -w 90% -h 90% -E ${lib.getExe config.programs.jjui.package}
+      bind C-t display-popup -d "#{pane_current_path}" -w 60% -h 60% -E ${lib.getExe config.programs.zsh.package}
+      bind C-s display-popup -w 90% -h 90% -E ${lib.getExe pkgs.lazyssh}
+    '';
+
+    programs.btop.package = lib.mkForce pkgs.btop-rocm;
+
+    sops.age.keyFile = lib.mkOverride 10 ("/persistent" + "${config.xdg.configHome}/sops/age/keys.txt");
+
+    sops.secrets."gsconnect/private.pem".sopsFile = lib.path.append ../../hosts/benedick "benedick.sops.yaml";
   };
 }
