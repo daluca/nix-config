d2996acc53ab5bb31dd0d65f55f476d9bb9e5ae0
Lucas Slebos <lucas@slebos.nz>
2026-08-08 09:51:26 +0200

    update gsconnect to be a module

---
+++ modules/gsconnect/default.nix
@@ -0,0 +1,184 @@
+{ self, ... }:
+
+{
+  flake.nixosModules.gsconnect = {
+    imports = with self.nixosModules; [
+      gsconnect-firewall
+    ];
+
+    home-manager.users.daluca.imports = with self.homeManagerModules; [
+      gsconnect
+    ];
+  };
+
+  flake.homeManagerModules.gsconnect = { config, lib, pkgs, ... }:
+    let
+      zenphone-10 = "1585fe42_d731_4e18_a83b_2f420174b038";
+    in
+    with pkgs.gnomeExtensions;
+    {
+      home.packages = [ gsconnect ];
+
+      dconf.settings."org/gnome/shell" = {
+        enabled-extensions = [ gsconnect.extensionUuid ];
+      };
+
+      dconf.settings."org/gnome/shell/extensions/gsconnect" = {
+        enabled = true;
+        devices = [
+          zenphone-10
+        ];
+      };
+
+      dconf.settings."org/gnome/shell/extensions/gsconnect/device/${zenphone-10}" = with lib.hm.gvariant; {
+        type = "phone";
+        paired = true;
+        certificate-pem = builtins.readFile ./zenphone-10.pem;
+        disabled-plugins = mkTuple [ ];
+        incoming-capabilities = [
+          "kdeconnect.battery"
+          "kdeconnect.bigscreen.stt"
+          "kdeconnect.clipboard"
+          "kdeconnect.clipboard.connect"
+          "kdeconnect.contacts.request_all_uids_timestamps"
+          "kdeconnect.contacts.request_vcards_by_uid"
+          "kdeconnect.findmyphone.request"
+          "kdeconnect.mousepad.keyboardstate"
+          "kdeconnect.mousepad.request"
+          "kdeconnect.mpris"
+          "kdeconnect.mpris.request"
+          "kdeconnect.notification"
+          "kdeconnect.notification.action"
+          "kdeconnect.notification.reply"
+          "kdeconnect.notification.request"
+          "kdeconnect.ping"
+          "kdeconnect.runcommand"
+          "kdeconnect.sftp.request"
+          "kdeconnect.share.request"
+          "kdeconnect.share.request.update"
+          "kdeconnect.sms.request"
+          "kdeconnect.sms.request_attachment"
+          "kdeconnect.sms.request_conversation"
+          "kdeconnect.sms.request_conversations"
+          "kdeconnect.systemvolume"
+          "kdeconnect.telephony.request"
+          "kdeconnect.telephony.request_mute"
+        ];
+        outgoing-capabilities = [
+          "kdeconnect.battery"
+          "kdeconnect.bigscreen.stt"
+          "kdeconnect.clipboard"
+          "kdeconnect.clipboard.connect"
+          "kdeconnect.connectivity_report"
+          "kdeconnect.contacts.response_uids_timestamps"
+          "kdeconnect.contacts.response_vcards"
+          "kdeconnect.findmyphone.request"
+          "kdeconnect.mousepad.echo"
+          "kdeconnect.mousepad.keyboardstate"
+          "kdeconnect.mousepad.request"
+          "kdeconnect.mpris"
+          "kdeconnect.mpris.request"
+          "kdeconnect.notification"
+          "kdeconnect.notification.request"
+          "kdeconnect.ping"
+          "kdeconnect.presenter"
+          "kdeconnect.runcommand.request"
+          "kdeconnect.sftp"
+          "kdeconnect.share.request"
+          "kdeconnect.sms.attachment_file"
+          "kdeconnect.sms.messages"
+          "kdeconnect.systemvolume.request"
+          "kdeconnect.telephony"
+        ];
+        supported-plugins = mkTuple [
+          "battery"
+          "clipboard"
+          "connectivity_report"
+          "contacts"
+          "findmyphone"
+          "mousepad"
+          "mpris"
+          "notification"
+          "ping"
+          "presenter"
+          "runcommand"
+          "sftp"
+          "share"
+          "sms"
+          "systemvolume"
+          "telephony"
+        ];
+      };
+
+      dconf.settings."org/gnome/shell/extensions/gsconnect/device/${zenphone-10}/plugin/battery" = with lib.hm.gvariant; {
+        custom-battery-notification-value = mkUint32 80;
+      };
+
+      dconf.settings."org/gnome/shell/extensions/gsconnect/device/${zenphone-10}/plugin/clipboard" = {
+        receive-content = true;
+        send-content = true;
+      };
+
+      dconf.settings."org/gnome/shell/extensions/gsconnect/device/${zenphone-10}/plugin/notification" = {
+        applications = builtins.toJSON {
+          Power = {
+            iconName = "org.gnome.Settings-power-symbolic";
+            enabled = true;
+          };
+          Clocks = {
+            iconName = "org.gnome.clocks";
+            enabled = true;
+          };
+          Color = {
+            iconName = "org.gnome.Settings-color-symbolic";
+            enabled = true;
+          };
+          "File Roller" = {
+            iconName = "org.gnome.FileRoller";
+            enabled = true;
+          };
+          Printers = {
+            iconName = "org.gnome.Settings-printers-symbolic";
+            enabled = true;
+          };
+          "Disk Usage Analyzer" = {
+            iconName = "org.gnome.baobab";
+            enabled = true;
+          };
+          Files = {
+            iconName = "org.gnome.Nautilus";
+            enabled = true;
+          };
+          Console = {
+            iconName = "org.gnome.Console";
+            enabled = true;
+          };
+          Disks = {
+            iconName = "org.gnome.DiskUtility";
+            enabled = true;
+          };
+          "Events and Tasks Reminders" = {
+            iconName = "org.gnome.Evolution-alarm-notify";
+            enabled = true;
+          };
+          "Date & Time" = {
+            iconName = "org.gnome.Settings-time-symbolic";
+            enabled = true;
+          };
+        };
+      };
+
+      dconf.settings."org/gnome/shell/extensions/gsconnect/device/${zenphone-10}/plugin/share" = {
+        receive-directory = "${config.home.homeDirectory}/Downloads";
+      };
+
+      xdg.mimeApps.defaultApplicationPackages = [
+        gsconnect
+      ];
+
+      xdg.configFile."gsconnect/certificate.pem".source = lib.path.append ../users/daluca "certificate.pem";
+
+      # TODO: Add back in with each hosts own key
+      # sops.secrets."gsconnect/private.pem".path = "${config.xdg.configHome}/gsconnect/private.pem";
+  };
+}
---
+++ modules/gsconnect/firewall.nix
@@ -0,0 +1,16 @@
+{
+  flake.nixosModules.gsconnect-firewall = { config, lib, pkgs, ... }: {
+    networking.firewall = rec {
+      allowedTCPPortRanges =
+        lib.mkIf
+          (builtins.elem pkgs.gnomeExtensions.gsconnect config.home-manager.users.daluca.home.packages)
+          [
+            {
+              from = 1714;
+              to = 1764;
+            }
+          ];
+      allowedUDPPortRanges = allowedTCPPortRanges;
+    };
+  };
+}
