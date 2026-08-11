e6d4f3e5d3321f68120905f1a06d200ef6ac91fd
Lucas Slebos <lucas@slebos.nz>
2026-08-11 23:38:28 +0200

    update mangohud to use home-manager options

--- modules/mangohud.nix
+++ modules/mangohud.nix
@@ -1,17 +1,26 @@
 {
   flake.homeManagerModules.mangohud = { pkgs, ... }: {
+    programs.mangohud = {
+      enable = true;
+      settings = {
+        no_display = true;
+      };
+      settingsPerApplication = {
+        wine-Cyberpunk2077 = {
+          preset = 1;
+          position = "top-right";
+          no_display = true;
+        };
+      };
+    };
+
     home.packages = with pkgs; [
       mangohud
     ];
 
-    xdg.configFile."MangoHud/MangoHud.conf".text = ''
-      no_display
-    '';
-
-    xdg.configFile."MangoHud/wine-Cyberpunk2077.conf".text = ''
-      preset=1
-      position=top-right
-      no_display
-    '';
+    # NOTE: catppuccin completely overwrite mangohud config
+    # No current work arounds
+    # https://github.com/catppuccin/nix/issues/999
+    catppuccin.mangohud.enable = false;
   };
 }
