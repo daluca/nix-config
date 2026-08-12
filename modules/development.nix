9dd633327edca197b6be8694d668415ccc249fe8
Lucas Slebos <lucas@slebos.nz>
2026-08-12 21:09:20 +0200

    add github web flow key to development module

--- modules/development.nix
+++ modules/development.nix
@@ -1,12 +1,22 @@
 { self, ... }:
 
 {
-  flake.homeManagerModules.development = { ... }: {
+  flake.homeManagerModules.development = { pkgs, ... }: {
     imports = with self.homeManagerModules; [
       direnv
       gardenTools
       just
       opentofu
+    ];
+
+    programs.gpg.publicKeys = [
+      {
+        source = pkgs.fetchurl {
+          url = "https://github.com/web-flow.gpg";
+          hash = "sha256-bor2h/YM8/QDFRyPsbJuleb55CTKYMyPN4e9RGaj74Q=";
+        };
+        trust = "full";
+      }
     ];
 
     home.persistence.home.directories = [
