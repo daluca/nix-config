a4e5a344d0e9107c1211821450df44ad87a48459
Lucas Slebos <lucas@slebos.nz>
2026-08-17 20:05:07 +0200

    update yazi to show hidden files

--- modules/yazi.nix
+++ modules/yazi.nix
@@ -2,6 +2,9 @@
   flake.homeManagerModules.yazi = {
     programs.yazi = {
       enable = true;
+      settings = {
+        mgr.show_hidden = true;
+      };
     };
 
     catppuccin.yazi.enable = true;
