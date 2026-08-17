3357c428fedd720b257a2dd1a7a1cfd2ef53e981
Lucas Slebos <lucas@slebos.nz>
2026-08-17 20:05:07 +0200

    update nixpkgs-unstable overlay to be a module

---
+++ modules/nixpkgs-unstable.nix
@@ -0,0 +1,10 @@
+{ inputs, ... }:
+
+{
+  flake.overlays.nixpkgs-unstable = final: _prev: {
+    unstable = import inputs.nixpkgs-unstable {
+      system = final.stdenv.hostPlatform.system;
+      config.allowUnfree = true;
+    };
+  };
+}
--- overlays/default.nix
+++ overlays/default.nix
@@ -107,36 +107,4 @@
           };
         };
     };
-
-  unstable-packages =
-    final: _prev:
-    with inputs;
-    let
-      inherit (final.stdenv.hostPlatform) system;
-    in
-    {
-      unstable = import inputs.nixpkgs-unstable {
-        inherit system;
-        config.allowUnfree = true;
-        overlays = [
-          (final: prev: {
-            deploy-rs = prev.deploy-rs.overrideAttrs {
-              version = "0.1.0-unstable-2025-09-01";
-
-              src = prev.fetchFromGitHub {
-                owner = "serokell";
-                repo = "deploy-rs";
-                rev = "125ae9e3ecf62fb2c0fd4f2d894eb971f1ecaed2";
-                hash = "sha256-N9gBKUmjwRKPxAafXEk1EGadfk2qDZPBQp4vXWPHINQ=";
-              };
-
-              postPatch = /* bash */ ''
-                substituteInPlace src/cli.rs \
-                  --replace 'version = "1.0"' 'version = "0.1.0"'
-              '';
-            };
-          })
-        ];
-      };
-    };
 }
