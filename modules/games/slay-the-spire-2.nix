1ebab8d27c79f17db9f961dfa4981c46a2948ea4
Lucas Slebos <lucas@slebos.nz>
2026-08-17 20:05:07 +0200

    add Slay the Spire 2 mangohud config

--- modules/games/slay-the-spire-2.nix
+++ modules/games/slay-the-spire-2.nix
@@ -1,5 +1,13 @@
 {
   flake.homeManagerModules.games-slayTheSpire2 = {
+    programs.mangohud.settingsPerApplication = {
+      SlayTheSpire2 = {
+        preset = 1;
+        position = "top-left";
+        no_display = true;
+      };
+    };
+
     home.persistence.home.directories = [
       ".local/share/SlayTheSpire2"
     ];
