7b9eb5d966d73817002d7dd5723aed8200253400
Lucas Slebos <lucas@slebos.nz>
2026-08-17 20:05:07 +0200

    update local content share to be a module

---
+++ modules/local-content-share.nix
@@ -0,0 +1,8 @@
+{
+  flake.nixosModules.localContentShare = {
+    services.local-content-share = {
+      enable = true;
+      listenAddress = "127.0.0.1";
+    };
+  };
+}
--- nixos/local-content-share/default.nix
+++
@@ -1,6 +0,0 @@
-{
-  services.local-content-share = {
-    enable = true;
-    listenAddress = "127.0.0.1";
-  };
-}
