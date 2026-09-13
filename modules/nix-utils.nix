5a2de2ebb1038e3bd1236354d820534506503817
Lucas Slebos <lucas@slebos.nz>
2026-09-13 14:50:14 +0200

    remove unused inputs in with statement

--- modules/nix-utils.nix
+++ modules/nix-utils.nix
@@ -1,14 +1,11 @@
-{ inputs, ... }:
+{ self, ... }:
 
 {
   flake.homeManagerModules.nix-utils = { pkgs, ... }: {
-    imports =
-      with inputs;
-      with inputs.self.homeManagerModules;
-      [
-        nh
-        comma
-      ];
+    imports = with self.homeManagerModules; [
+      nh
+      comma
+    ];
 
     home.packages = with pkgs; [
       nix-inspect
