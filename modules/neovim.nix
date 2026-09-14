7475e18d11ce7ee124b658b728c5304b538cfce3
Lucas Slebos <lucas@slebos.nz>
2026-09-15 00:06:39 +0200

    update neovide to be a module

--- modules/neovim.nix
+++ modules/neovim.nix
@@ -38,4 +38,39 @@
         ".local/share/nvim"
       ];
     };
+
+  flake.homeManagerModules.neovide = { config, pkgs, ... }: {
+    programs.neovide = {
+      enable = true;
+      package = pkgs.unstable.neovide;
+      settings = {
+        font =
+          let
+            font = "Monaspace Krypton Var";
+          in
+          {
+            normal = [ font ];
+            size = 12;
+            features.${font} = [
+              "+calt"
+              "+ss01"
+              "+ss02"
+              "+ss03"
+              "+ss04"
+              "+ss05"
+              "+ss06"
+              "+ss07"
+              "+ss08"
+              "+ss09"
+              "+ss10"
+              "+liga"
+            ];
+          };
+      };
+    };
+
+    xdg.mimeApps.defaultApplicationPackages = with config; [
+      programs.neovide.package
+    ];
+  };
 }
