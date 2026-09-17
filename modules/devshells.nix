8df886e39cdee8d39fe9f51c60dba610327cd967
Lucas Slebos <lucas@slebos.nz>
2026-09-17 14:50:40 +0200

    add pre-commit packages to devshell

--- modules/devshells.nix
+++ modules/devshells.nix
@@ -7,16 +7,19 @@
     }:
     {
       devShells.default = pkgs.mkShell {
+        inherit (self'.checks.pre-commit) shellHook;
         name = "nix-config";
-        packages = with pkgs; [
-          sops
-          git-agecrypt
-          just
-          deploy-rs
-          colmena
-        ];
+        packages =
+          with pkgs;
+          self'.checks.pre-commit.enabledPackages
+          ++ [
+            sops
+            git-agecrypt
+            just
+            deploy-rs
+            colmena
+          ];
         JUST_COMMAND_COLOR = "blue";
-        shellHook = self'.checks.pre-commit.shellHook;
       };
     };
 }
