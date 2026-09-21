572efdaa920f760125563885e6be1581c601be60
Lucas Slebos <lucas@slebos.nz>
2026-09-21 11:33:13 +0200

    add local nodes as substituters to desktop machines

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -15,6 +15,11 @@
       trusted-public-keys = [
         "nixos-raspberrypi.cachix.org-1:4iMO9LXa8BqhU+Rpg6LQKiGa2lsNh/j2oiYLNOQ5sPI="
       ];
+      substituters = [
+        "https://nix-community.cachix.org?priority=50"
+        "ssh-ng://remotebuild@dalaran?priority=100&ssh-key=/etc/ssh/ssh_host_ed25519_key"
+        "ssh-ng://daluca@stormwind?priority=110&ssh-key=/home/daluca/.ssh/id_ed25519"
+      ];
     };
 
     deploy.tags = [
