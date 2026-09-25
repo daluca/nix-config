4c467a3266043bab5041470fefbe94c6ffa54b47
Lucas Slebos <lucas@slebos.nz>
2026-09-25 15:54:00 +0200

    update unifi controller to be a module

---
+++ modules/unifi.nix
@@ -0,0 +1,17 @@
+{
+  flake.nixosModules.unifi-controller = { lib, pkgs, ... }: {
+    services.unifi = {
+      enable = true;
+      mongodbPackage = pkgs.mongodb-ce;
+      openFirewall = true;
+    };
+
+    systemd.services.unifi = {
+      environment.LD_LIBRARY_PATH = lib.mkForce "${pkgs.stdenv.cc.cc.lib}/lib:${pkgs.systemdLibs}/lib";
+    };
+
+    nixpkgs.config.permittedInsecurePackages = [
+      "unifi-controller-9.5.21"
+    ];
+  };
+}
